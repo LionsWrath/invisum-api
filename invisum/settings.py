88bc0cf1b461268acf10939f73cc23f9004a2b26
lionswrath <caio.tonetti@gmail.com>
2017-03-14 01:49:32 -0300

    Added TravisCI support

--- invisum/settings.py
+++ invisum/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'invisum.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.mysql',
+            'NAME':     'travisdb',
+            'USER':     'mysql',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
     }
-}
+else:
+    DATABASES = {
+	'default': {
+	    'ENGINE': 	'django.db.backends.sqlite3',
+	    'NAME': 	os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
+    }
 
 # Media
 # https://docs.djangoproject.com/en/1.10/ref/settings/#media-root
