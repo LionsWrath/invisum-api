b380044b8906800d1d3fc3da0d668e9312c574ab
lionswrath <caio.tonetti@gmail.com>
2017-03-21 10:06:08 -0300

    Added DELETE tests on Dataset and Personal

--- datasets/tests.py
+++ datasets/tests.py
@@ -9,6 +9,9 @@ from os import path
 
 files_url = path.join(settings.MEDIA_ROOT, 'test')
 
+# Status codes: http://www.django-rest-framework.org/api-guide/status-codes
+
+# LIST, RETRIEVE
 class UserTest(APITestCase):
     @classmethod
     def setUpClass(self):
@@ -46,7 +49,7 @@ class UserTest(APITestCase):
         data = '{"id":1,"email":"lennon@thebeatles.com","username":"john","datasets":[]}'
         self.assertEqual(response.content, data)
 
-# POST
+# POST, DELETE
 class DatasetTest(APITestCase):
     @classmethod
     def setUpClass(self):
@@ -68,7 +71,6 @@ class DatasetTest(APITestCase):
         response = self.client.post(reverse('dataset-list'), dataset_data_2)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Dataset.objects.count(), 1)
 
     def test_dataset_create_2(self):
         datapath = path.join(files_url, 'usagov_test.txt')
@@ -84,11 +86,48 @@ class DatasetTest(APITestCase):
         response = self.client.post(reverse('dataset-list'), dataset_data_1)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Dataset.objects.count(), 1)
 
-# 403 error for not logged (FORBIDDEN)
-# 400 error for malformed request (BAD RESQUEST)
-# POST
+    def test_dataset_delete_1(self):
+        datapath = path.join(files_url, 'usagov_test.txt')
+        f = file(datapath)
+        dataset_data_1 = {
+            "title": "Bitly data from USA government",
+            "about": "",
+            "data": SimpleUploadedFile(f.name, f.read()),
+            "extension": 2
+        }
+
+        self.client.login(username='john', password='johnpassword')
+        self.client.post(reverse('dataset-list'), dataset_data_1)
+
+        self.assertEqual(Dataset.objects.count(), 1)
+        id = Dataset.objects.all()[0].id
+
+        response = self.client.delete(reverse('dataset-detail', args=[id]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+ 
+    def test_dataset_delete_2(self):
+        datapath = path.join(files_url, 'usagov_test.txt')
+        f = file(datapath)
+        dataset_data_1 = {
+            "title": "Bitly data from USA government",
+            "about": "",
+            "data": SimpleUploadedFile(f.name, f.read()),
+            "extension": 2
+        }
+
+        self.client.login(username='john', password='johnpassword')
+        self.client.post(reverse('dataset-list'), dataset_data_1)
+        self.client.logout()
+
+        self.assertEqual(Dataset.objects.count(), 1)
+        id = Dataset.objects.all()[0].id
+
+        self.client.login(username='jair', password='jairpassword')
+        response = self.client.delete(reverse('dataset-detail', args=[id]))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+# POST, DELETE
 class PersonalDatasetTest(APITestCase):
     @classmethod
     def setUpClass(self):
@@ -124,6 +163,9 @@ class PersonalDatasetTest(APITestCase):
         self.client.post(reverse('dataset-list'), dataset_data_1)
         self.client.logout()
 
+    def tearDown(self):
+        Dataset.objects.all().delete()
+
     def test_personal_create_1(self):
         data = { "description": "pdataset" }
 
@@ -145,8 +187,40 @@ class PersonalDatasetTest(APITestCase):
         response = self.client.post(reverse('personal-create', args=[id]), data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    
-    
+
+    # A user try to delete a PersonalDataset
+    def test_personal_delete_1(self):
+        data = { "description": "pdataset" }
+
+        self.assertEqual(Dataset.objects.count(), 2)
+        id = Dataset.objects.all()[0].id
+
+        self.client.login(username='jair', password='jairpassword')
+        self.client.post(reverse('personal-create', args=[id]), data)
+        
+        self.assertEqual(PersonalDataset.objects.count(), 1)
+        id = PersonalDataset.objects.all()[0].id
+
+        response = self.client.delete(reverse('personal-detail', args=[id])) 
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+ 
+    # Another user try to delete a PersonalDataset
+    def test_personal_delete_2(self):
+        data = { "description": "pdataset" }
+
+        self.assertEqual(Dataset.objects.count(), 2)
+        id = Dataset.objects.all()[0].id
+
+        self.client.login(username='jair', password='jairpassword')
+        self.client.post(reverse('personal-create', args=[id]), data)
+        self.client.logout()
+        
+        self.assertEqual(PersonalDataset.objects.count(), 1)
+        id = PersonalDataset.objects.all()[0].id
+
+        self.client.login(username='john', password='johnpassword')
+        response = self.client.delete(reverse('personal-detail', args=[id])) 
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 class RatingTest(APITestCase):
     pass
